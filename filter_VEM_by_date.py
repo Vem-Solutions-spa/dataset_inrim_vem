683441c4e18affb114e8f3978816c79a1d58dcf4
Michele Morello <michele@Micheles-MacBook-Air.local>
2018-07-09 15:35:55 +0200

    filter date change

--- filter_VEM_by_date.py
+++ filter_VEM_by_date.py
@@ -8,9 +8,9 @@ vem_iter = pd.read_csv('../Datasets/VEM/top_2018_01_019_2018_01_23.csv', chunksi
                         parse_dates=['date'],infer_datetime_format=True, date_parser=pd.to_datetime)
 
 
-start_date = pd.to_datetime("2018-01-19 00.00", unit='s')
-end_date = pd.to_datetime("1516702403", unit='s')
+# start_date = pd.to_datetime("2018-01-19 00:00:01", infer_datetime_format=True)
+start_date = pd.to_datetime("2018-01-19 00:00:01", format="%Y-%m-%d %H:%M:%S")
+end_date = pd.to_datetime("2018-01-19 23:59:59", format="%Y-%m-%d %H:%M:%S")
 
-start_date
 
 df_vem = pd.concat([chunk[(chunk['date'] > start_date) & (chunk['date'] < end_date)]for chunk in vem_iter])
